b9dbf6a5e435dec3f3b834f88ec2cdef5a9deeea
MartaMariz <61186044+MartaMariz@users.noreply.github.com>
2022-01-17 23:54:44 +0000

    added pc random move

--- breakthrough.pl
+++ breakthrough.pl
@@ -1,5 +1,6 @@
 
 :- use_module(library(lists)).
+:- use_module(library(random)).
 
 columnToInt('A', 0).
 columnToInt('B', 1).
@@ -12,24 +13,26 @@ columnToInt('H', 7).
 
 
 
-printInitialBoard :-
-    initialBoard(X),
+initial_state(GameState) :-
+    initialBoard(GameState),
+    display_game(GameState).
+
+
+display_game(X) :-
     lineNumbers(Y),
-    printBoard(X,Y).
+    display_game(X,Y).
 
-
-
-printBoard([],[]) :-
+display_game([],[]) :-
     write('  |---|---|---|---|---|---|---|---|'), nl,
     write('    A   B   C   D   E   F   G   H  '), nl.
 
-printBoard([Line|Board],[LineNumb|Remainder]) :-
+display_game([Line|Board],[LineNumb|Remainder]) :-
     write('  |---|---|---|---|---|---|---|---|'), nl,
     %write('  '),  nl,
     write(LineNumb), write(' '),
     printLine(Line),
     write('|'), nl,
-    printBoard(Board,Remainder).
+    display_game(Board,Remainder).
 
 /* Recursive function to print each board's line */
 printLine([]).
@@ -57,7 +60,8 @@ translate(0,' . ').
 translate(1,' W '). 
 translate(2,' B '). 
 
-getPiecePos(Column, Row):-
+get_piece_pos(Column, Row):-
+    repeat,
     write('Choose which piece to move.'), nl,
 	write('Column:'),
 	get_char(Char),skip_line,
@@ -70,7 +74,8 @@ getPiecePos(Column, Row):-
 
 
 
-getDestPos(Column, Row):-
+get_dest_pos(Column, Row):-
+    repeat,
     write('Choose where to move.'), nl,
 	write('Column:'),
 	get_char(Char),skip_line,
@@ -95,7 +100,7 @@ changePlayer(1,2).
 changePlayer(2,1).
 
 
-checkPiecePos(Column, Row, GameState, Player):- 
+check_piece_pos(Column, Row, GameState, Player):- 
     nth0(Row, GameState, RowList),
     nth0(Column, RowList, Piece),
     Player =:= Piece.
@@ -103,64 +108,131 @@ checkPiecePos(Column, Row, GameState, Player):-
     
 initialPlayer(1).
 
-
-checkResult(Piece, _ , _, Player, _, _):- Piece =:= Player ,!.
-checkResult(Piece, Row, Col, Player,  GameState, UpdatedGameState):-
-    replace(GameState, Row, Col, Player, UpdatedGameState ).
-
-
 replace( [L|Ls] , 0 , C , Piece , [R|Ls] ) :- 
-  replaceRow(L,C,Piece,R).                                      
+  replace_row(L,C,Piece,R).                                      
 replace( [L|Ls] , R , C , Piece , [L|Rs] ) :- 
   R > 0 ,                                
   R1 is R-1 ,                             
   replace( Ls , R1 , C , Piece , Rs ).                                       
 
-replaceRow( [_|Cs] , 0 , Piece , [Piece|Cs] ) .  
-replaceRow( [H|Cs] , C , Piece , [H|Rs] ) :- 
+replace_row( [_|Cs] , 0 , Piece , [Piece|Cs] ) .  
+replace_row( [H|Cs] , C , Piece , [H|Rs] ) :- 
   C > 0 ,                                  
   C1 is C-1 ,                                
-  replaceRow( Cs , C1 , Piece , Rs ). 
+  replace_row( Cs , C1 , Piece , Rs ). 
 
-checkCollumn(CI, CF,_):- CF =:= CI - 1.
-checkCollumn(CI, CF,_):- CF =:= CI + 1.
-checkCollumn(CI, CF, Piece):- CF =:= CI, Piece =:= 0 .
+check_collumn(CI, CF, _):- CF =:= CI - 1 .
+check_collumn(CI, CF, _):- CF =:= CI + 1 .
+check_collumn(CI, CF, Piece):- CF =:= CI, Piece =:= 0 .
 
-
-checkMove(CI, RI, CF, RF,1, GameState, UpdatedGameState):-
+check_valid([CI-RI, CF-RF] ,1 , GameState):-
     nth0(RF, GameState, RowList),
     nth0(CF, RowList, Piece),
+    dif(1, Piece),
     RF =:= RI - 1,
-    checkCollumn(CI, CF, Piece),
-    checkResult(Piece, RF, CF, 1, GameState, FinalState),
-    replace(FinalState, RI, CI, 0, UpdatedGameState).    
+    check_collumn(CI, CF, Piece).
 
-checkMove(CI, RI, CF, RF,2, GameState, UpdatedGameState):-
-    RF =:= RI + 1,
-    checkCollumn(CI, CF),
+check_valid([CI-RI, CF-RF] ,2 , GameState):-
     nth0(RF, GameState, RowList),
     nth0(CF, RowList, Piece),
-    checkResult(Piece, RF, CF, 2, GameState, FinalState),
-    replace(FinalState, RI, CI, 0, UpdatedGameState). 
+    dif(2, Piece),
+    check_collumn(CI, CF, Piece),
+    RF =:= RI + 1.
+
+move([CI-RI, CF-RF],Player , GameState, UpdatedGameState):-
+    check_valid([CI-RI, CF-RF], Player, GameState),
+    replace(GameState, RF, CF, Player, MidGameState ),
+    replace(MidGameState, RI, CI, 0, UpdatedGameState).    
 
 
+valid_set_pos(CI,RI,CF,RF,GameState,Player):-
+    check_piece_pos(CI,RI, GameState, Player),
+    check_valid( [CI-RI, CF-RF], Player, GameState).
+
+
+
+valid_moves(GameState, Player, ListOfMoves):-
+    findall([CI-RI, CF-RF], valid_set_pos(CI,RI,CF,RF,GameState,Player), ListOfMoves).
+
+printa([]).
+printa([[A-B, C-D]|T]):-
+    write(A-B), write(C-D),nl,
+    printa(T).
 
 play:-
-    initialBoard(GameState),
-    lineNumbers(Y),
-    printBoard(GameState, Y),
+    initial_state(GameState),
     initialPlayer(Player),
-    move(Player, GameState, Y).
-    
+    play_pc(Player, GameState).
 
-
-move(Player, GameState, Y):-
+get_move(Player,GameState, [CI-RI, CF-RF]):-
     write('Player '), write(Player), write(', it is your turn!'), nl,
-    getPiecePos(CI, RI),
-    checkPiecePos(CI,RI, GameState, Player),
-    getDestPos(CF, RF),
-    checkMove(CI, RI, CF, RF,Player, GameState, UpdatedGameState),
-    printBoard(UpdatedGameState,Y),
+    get_piece_pos(CI, RI),
+    check_piece_pos(CI,RI, GameState, Player),
+    get_dest_pos(CF, RF).
+
+
+check_end([H | T], Player):-
+    H =:= Player.
+
+check_end([H | T], Player):-
+    check_end( T, Player).
+
+check_end([], Player):- fail.
+
+
+game_over([First|_], 2, Winner):-
+    check_end(First, 1),!,
+    Winner is  1.
+
+game_over(GameState, 1, Winner):-
+    length(GameState, NumRows),
+    LastRow is NumRows-1,
+    nth0( LastRow , GameState, RowList),
+    check_end(RowList, 2),!,
+    Winner is 2.
+
+game_over(_, _, Winner):-
+    Winner is -1.
+
+
+play_pc(Player, GameState):-
+    game_over(GameState, Player, Winner),
+    dif(Winner, -1),!,
+    write('Player '), write(Winner), write(' won!!').
+
+play_pc(Player, GameState):-
+    get_move(Player,GameState, Move),
+    move(Move ,Player, GameState, UpdatedGameState),
+    display_game(UpdatedGameState),
     changePlayer(Player,Oponent),
-    move(Oponent, UpdatedGameState, Y).
+    play_pc_turn(Oponent, UpdatedGameState).
+
+
+play_pc_turn(Player, GameState):-
+    game_over(GameState, Player, Winner),
+    dif(Winner, -1),!,
+    write('Player '), write(Winner), write(' won!!').
+
+play_pc_turn(Player, GameState):-
+    valid_moves(GameState, Player, ListOfMoves),
+    length(ListOfMoves, Length),
+    random( 0, Length, Result),
+    nth0( Result , ListOfMoves, Move),
+    move(Move ,Player, GameState, UpdatedGameState),
+    display_game(UpdatedGameState),
+    changePlayer(Player,Oponent),
+    play_pc(Oponent, UpdatedGameState).
+
+
+play_pp(Player, GameState):-
+    game_over(GameState, Player, Winner),
+    dif(Winner, -1),!,
+    write('Player '), write(Winner), write(' won!!').
+
+play_pp(Player, GameState):-
+    get_move(Player,GameState, Move),
+    move(Move ,Player, GameState, UpdatedGameState),
+    display_game(UpdatedGameState),
+    changePlayer(Player,Oponent),
+    play_pp(Oponent, UpdatedGameState).
 
